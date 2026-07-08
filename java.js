8a9e4d4043112ffcc94eed8a9a9bead918f8a93e
maivannhat1 <maivannhat1107@gmail.com>
2026-07-08 18:49:36 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,19 @@
+function upDate(previewPic) {
+
+    console.log(previewPic);
+
+    document.getElementById("image").innerHTML = previewPic.alt;
+
+    document.getElementById("image").style.backgroundImage =
+        "url('" + previewPic.src + "')";
+
+}
+
+function unDo() {
+
+    document.getElementById("image").style.backgroundImage = "url('')";
+
+    document.getElementById("image").innerHTML =
+        "Di chuột qua một hình ảnh bên dưới để hiển thị ở đây";
+
+}
